423199b0983c0de5a9463a52858cc1db062b31c5
quotely-co <quotely7@gmail.com>
2025-02-21 23:54:26 +0530

    updated the errorin react

--- src/components/Customer/SingleShop.jsx
+++ src/components/Customer/SingleShop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import { PlusIcon, MinusIcon, XIcon } from 'lucide-react';
