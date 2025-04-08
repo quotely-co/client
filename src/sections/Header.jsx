ed02a9d8837ca27c8b4b6064184871f7597885db
shaamiilll <shamilamiyan@gmail.com>
2025-04-08 11:29:28 +0530

    Updated the logo typo error

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
