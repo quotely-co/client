cdcff5d63632e7e0757bd56b8b5f81b97b11d547
shaamiilll <shamilamiyan@gmail.com>
2025-04-08 11:46:00 +0530

    Updated the footer error in logo

--- src/pages/factory/Step3.jsx
+++ src/pages/factory/Step3.jsx
@@ -122,13 +122,12 @@ const Step3 = ({ prevStep }) => {
 
       let response;
       
-      alert(import.meta.env.VITE_HOST_URL)
       // Handle different payment gateways
       switch(paymentMethod) {
         
         case "stripe":
           response = await axios.post(
-            `${import.meta.env.VITE_HOST_URL}/api/payment/create-checkout-session`,
+            `https://api.quotely.shop/api/payment/create-checkout-session`,
             { 
               selectedPlan: { 
                 priceId: selectedPlan.priceId,
